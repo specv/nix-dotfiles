09fab6f29a1e3e41feb54edd40b862779c181b21
specv <61646426+specv@users.noreply.github.com>
2022-03-06 22:24:10 +0800

    Configure zsh

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -17,7 +17,6 @@
     zsh
     python3
     socat
-    tree
     lsd
     bat
     direnv
@@ -26,6 +25,9 @@
     tmuxinator
     nodejs
     yarn
+    starship
+    # https://nixos.wiki/wiki/Fonts
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
 
   # This value determines the Home Manager release that your
@@ -40,6 +42,7 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
   programs.lazygit = {
     enable = true;
     settings = {
@@ -48,12 +51,103 @@
       };
     };
   };
+
   programs.z-lua = {
     enable = true;
     options = [ "enhanced" "once" "fzf" "echo" ];
     enableAliases = true;
   };
+
+  programs.fzf = {
+    enable = true;
+  };
+
   programs.zsh = {
     enable = true;
+    defaultKeymap = "viins";
+    enableAutosuggestions = true;
+    #enableSyntaxHighlighting = true;
+    sessionVariables = {
+      EDITOR = "vi";
+      VISUAL = "vi";
+
+      # tmux
+      TERM = "xterm-256color";
+
+      # locale
+      # make sure check iTerm2's "Set locale variables automatically" off also
+      LC_ALL = "en_US.UTF-8";
+      LANG = "en_US.UTF-8";
+    };
+    initExtra = ''
+      # profile
+      source ~/.bash_profile
+    '';
+    history = {
+      size = 10000000;
+      save = 10000000;
+      ignoreDups = true;
+      ignoreSpace = false;
+      expireDuplicatesFirst = true;
+      share = true;
+      extended = true;
+    };
+    shellAliases = {
+      vi   = "nvim";
+      lg   = "lazygit";
+      l    = "ls";
+      ls   = "lsd";
+      la   = "l -a";
+      ll   = "l -la";
+      tree = "l --tree";
+      cat  = "bat";
+    };
+    #plugins = [
+    #  {
+    #    # will source zsh-autosuggestions.plugin.zsh
+    #    name = "zsh-autosuggestions";
+    #    src = pkgs.fetchFromGitHub {
+    #      owner = "zsh-users";
+    #      repo = "zsh-autosuggestions";
+    #      rev = "v0.7.0";
+    #      sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+    #    };
+    #  }
+    #];
+    #oh-my-zsh = {
+    #  enable = true;
+    #  plugins = [
+    #    "git"
+    #    "ssh-agent"
+    #  ];
+    #};
+    #prezto = {
+    #  enable = true;
+    #  caseSensitive = false;
+    #  utility.safeOps = true;
+    #  editor = {
+    #    dotExpansion = true;
+    #    keymap = "vi";
+    #  };
+    #  pmodules = [
+    #    "autosuggestions"
+    #    "completion"
+    #    "directory"
+    #    "editor"
+    #    "git"
+    #    "terminal"
+    #  ];
+    #};
+  };
+
+  # The conda module shows the current Conda (opens new window)environment, if $CONDA_DEFAULT_ENV is set.
+  # This does not suppress conda's own prompt modifier, you may want to run conda config --set changeps1 False.
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      scan_timeout = 100;
+      command_timeout = 1000;
+    };
   };
 }
