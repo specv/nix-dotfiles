20abc0838b788ed15580de08f0e4d6ef4cbdeae5
specv <61646426+specv@users.noreply.github.com>
2022-04-05 01:32:40 +0800

    Separate out init.lua file from `programs.neovim.extraConfig`

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -368,41 +368,7 @@ in
       set nohlsearch
 
       lua << EOF
-        -- onedark color theme
-        require('onedark').setup {
-          style = 'deep'
-        }
-        require('onedark').load()
-
-        -- blazing fast and easy to configure Neovim statusline written in Lua
-        require('lualine').setup()
-
-        require('nvim-treesitter.configs').setup {
-          ensure_installed = "maintained",
-          -- consistent syntax highlighting (rainbow needs this option)
-          highlight = {
-            enable = true
-          },
-          -- indentation based on treesitter for the `=` operator
-          indent = { 
-            enable = true
-          },
-          -- nvim-ts-rainbow: rainbow parentheses using tree-sitter.
-          rainbow = {
-            enable = true,
-            extended_mode = true,
-            max_file_lines = nil,
-          },
-        }
-
-        -- adds indentation guides to all lines (including empty lines)
-        require('indent_blankline').setup {
-          show_current_context = true,
-          show_current_context_start = false,
-        }
-
-        -- smart and powerful comment plugin for neovim
-        require('Comment').setup()
+        ${lib.strings.fileContents ../dotfiles/init.lua}
       EOF
     '';
   };
