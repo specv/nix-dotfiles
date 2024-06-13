e869f59e8f26181b97288f5b989b6a1d7c9ebfb6
specv <61646426+specv@users.noreply.github.com>
2024-06-13 17:46:51 +0800

    Support `lazyvim` panel switch

--- config/skhd.zsh
+++ config/skhd.zsh
@@ -2,7 +2,7 @@ function paneable() {
   local window_info=$(yabai -m query --windows --window)
   IFS=',' read -r app title pid <<< $(echo $window_info | jq -r '[.app, .title, .pid] | join(",")')
 
-  if [[ "$app" == "kitty" ]] || [[ "$title" == "Zellij"* ]] || (ps -p $pid | grep -q "zellij"); then
+  if [[ "$app" == "kitty" ]] || ( [[ "$app" == "Alacritty" ]] && [[ "$title" == "LazyVim" ]] ) || [[ "$title" == "Zellij"* ]] || (ps -p $pid | grep -q "zellij"); then
     return 0
   else
     return 1
