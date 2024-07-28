817e00a36312236d5e0f3ed0418ee8098d16f74c
specv <61646426+specv@users.noreply.github.com>
2024-07-28 21:51:40 +0800

    skhd: switch same app across spaces

--- config/skhd.zsh
+++ config/skhd.zsh
@@ -8,3 +8,26 @@ function paneable() {
     return 1
   fi
 }
+
+function current_app_next_space() {
+  local current_window=$(yabai -m query --windows --window)
+  local current_app=$(echo $current_window | jq -r '.app')
+  local current_space=$(echo $current_window | jq -r '.space')
+  local app_spaces=(${(f)"$(yabai -m query --windows | jq -r ".[] | select(.app==\"$current_app\") | .space" | sort -n | uniq)"})
+
+  if [[ ${#app_spaces} -le 1 ]]; then
+    local next_space=$current_space
+  else
+    local current_index=${app_spaces[(i)$current_space]}
+
+    if [[ $current_index -gt ${#app_spaces} ]]; then
+      local next_space=$app_spaces[1]
+    else
+      local next_index=$(( (current_index % ${#app_spaces}) + 1 ))
+      local next_space=$app_spaces[$next_index]
+    fi
+  fi
+
+  local next_space_label=$(yabai -m query --spaces --space $next_space | jq -r '.label | .[-1:]')
+  echo $next_space_label
+}
